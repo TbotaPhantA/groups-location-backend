0be0126f63de14ed0d3ced0ef5665bad5ae05254
Bota <botakiller@mail.ru>
2021-07-13 01:02:27 +0300

    api swagger library for api documentation was added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Group Location API')
+        .setDescription('This is API has group and user and we can track all locations of users in every particular group, also we can track marks for an user')
+        .setVersion('0.0.1')
+        .addTag('group_location')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
