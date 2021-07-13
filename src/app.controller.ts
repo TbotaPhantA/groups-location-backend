4e36b8e5416b0ea815d21084e399d75b0ae3caac
Bota <botakiller@mail.ru>
2021-07-13 23:20:51 +0300

    deleted unnecessary files

--- src/app.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get } from "@nestjs/common";
-import { randomUUID } from "crypto";
-import { nanoid } from "nanoid";
-import { AppService } from "./app.servce";
-
-
-@Controller('/api')
-export class AppController { }
