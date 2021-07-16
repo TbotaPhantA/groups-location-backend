6b9f97b05db2c6487a95c8922a0906c3eb1e956a
Bota <botakiller@mail.ru>
2021-07-16 14:49:53 +0300

    name of the id field changed from ID to UUID

--- src/users/dto/get-user-output.dto.ts
+++ src/users/dto/get-user-output.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from "@nestjs/swagger"
 
 export class getUserOutput {
     @ApiProperty({ example: 'sdlfkjsldfk2234234', description: 'identificator of user' })
-    readonly id: string;
+    readonly uuid: string;
 
     @ApiProperty({ example: 'Zabor', description: 'user nickname' })
     readonly name: string;
