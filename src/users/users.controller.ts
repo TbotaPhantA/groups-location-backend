09d9dcd1994eac5a6e0403fcc177c1e876c909b6
Bota <botakiller@mail.ru>
2021-07-19 02:25:03 +0300

    the /dev/ division was added to routs, to show that, this routs can be used only with development purposes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,8 @@
 import { Body, Controller, Get, HttpStatus, Param, ParseUUIDPipe, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
-import { GetUserOutputDto } from './dto/get-user-output.dto';
 import { CreateUserInputDto } from './dto/create-user-input.dto';
+import { User } from './users.entity';
 
 @ApiBearerAuth()
 @ApiTags('Users')
@@ -11,36 +11,36 @@ export class UsersController {
 
     constructor(private usersService: UsersService) {}
 
-    @Get()
+    @Get('/dev/')
     @ApiOperation({summary: "Getting all the users"})
     @ApiResponse({
         status: 200, 
         description: 'All found users',
-        type: [GetUserOutputDto],
+        type: [User],
     })
-    getUsers(): Promise<GetUserOutputDto[]> {
+    getUsers(): Promise<User[]> {
         return this.usersService.getAllUsers();
     }
 
-    @Get(':uuid')
+    @Get('/dev/:uuid')
     @ApiOperation({summary: "Getting one user by his ID"})
     @ApiResponse({
         status: 200, 
         description: 'find user',
-        type: GetUserOutputDto,
+        type: User,
     })
-    getUser(@Param('uuid') uuid: string): Promise<GetUserOutputDto> {
+    getUser(@Param('uuid') uuid: string): Promise<User> {
         return this.usersService.getUserByUUID(uuid);
     }
 
-    @Post('/create')
+    @Post('/dev/create')
     @ApiOperation({ summary: "creating new user in the DB"})
     @ApiResponse({
         status: HttpStatus.CREATED,
         description: "Validation and creation of new user in the DB",
-        type: GetUserOutputDto,
+        type: User,
     })
-    createUser(@Body() dto: CreateUserInputDto): Promise<GetUserOutputDto> {
+    createUser(@Body() dto: CreateUserInputDto): Promise<User> {
         return this.usersService.createUser(dto);
     }
 }
