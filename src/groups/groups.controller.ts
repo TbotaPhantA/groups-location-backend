0ec272aa047f685ab866cfab3657218de734c16f
Bota <botakiller@mail.ru>
2021-07-19 20:24:24 +0300

    documentation of groups controller was deleted, because all methods here can't be used by the frontend, they are only can be needed with development purposes

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -3,31 +3,17 @@ import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagg
 import { GroupsService } from './groups.service';
 import { GetGroupOutput } from './dto/get-group-output.dto';
 
-@ApiBearerAuth()
-@ApiTags('Groups')
 @Controller('groups')
 export class GroupsController {
 
     constructor(private groupsService: GroupsService) {}
 
     @Get()
-    @ApiOperation({summary: "Getting all the gropus"})
-    @ApiResponse({
-        status: 200, 
-        description: 'All found users',
-        type: [GetGroupOutput],
-    })
     getUsers() {
         return this.groupsService.getGroups();
     }
 
     @Get(':id')
-    @ApiOperation({summary: "Getting one group by its ID"})
-    @ApiResponse({
-        status: 200, 
-        description: 'find user',
-        type: GetGroupOutput,
-    })
     getUser(@Param('uuid', new ParseUUIDPipe()) uuid: string) {
         return this.groupsService.getGroup(uuid);
     }
