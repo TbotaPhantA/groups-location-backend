6fbe6550ca3fcd4b48438204f41f331d6844290d
Bota <botakiller@mail.ru>
2021-07-20 19:42:25 +0300

    creation of the group was added and it's ready for the tests

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GroupsService } from './groups.service';
 import { GetGroupOutput } from './dto/get-group-output.dto';
+import { JwtGuard } from 'src/native-auth/jwt-auth.guard';
+import { Request } from 'express';
+import { GetAuthenticatedUser } from 'src/native-auth/get-user.decorator';
+import { User } from 'src/users/users.entity';
+import { CreateGroupInputDto } from './dto/create-group-input.dto';
 
 @Controller('groups')
 export class GroupsController {
 
     constructor(private groupsService: GroupsService) {}
+
+    @Post('create')
+    @UseGuards(JwtGuard)
+    createGroup(@GetAuthenticatedUser() user: User, @Body() dto: CreateGroupInputDto) {
+        return this.groupsService.createGroup(user, dto);
+    }
 
     @Get()
     getUsers() {
--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -6,10 +6,16 @@ import { Group } from './groups.entity';
 import { GroupsService } from './groups.service';
 import { UsersGroups } from './users_groups.entity';
 import { Location } from '../locations/locations.entity';
+import { JwtGuard } from 'src/native-auth/jwt-auth.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { NativeAuthModule } from 'src/native-auth/native-auth.module';
 
 @Module({
   controllers: [GroupsController],
   providers: [GroupsService],
-  imports: [TypeOrmModule.forFeature([Group, User, UsersGroups, Location])],
+  imports: [
+    TypeOrmModule.forFeature([Group, User, UsersGroups, Location]),
+    NativeAuthModule
+  ],
 })
 export class GroupsModule {}
