6b2c0c23f4003296d38b521029bf07694dc6815a
Bota <botakiller@mail.ru>
2021-07-20 17:08:43 +0300

    minor change

--- src/native-auth/jwt-auth.guard.ts
+++ src/native-auth/jwt-auth.guard.ts
@@ -26,7 +26,6 @@ export class JwtGuard implements CanActivate {
 
         try {
             const user = this.jwtService.verify(token)
-            request.user = user;
         } catch {throw new UnauthorizedException('Invalid Token, User is not authorized')}
 
         return true;
