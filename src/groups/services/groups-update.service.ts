682f2517b62674a60976e75aa139446715b375c3
Bota <botakiller@mail.ru>
2021-08-01 19:49:22 +0300

    invite link is disposable, so when someone uses this link, it's being deleted from our redis

--- src/groups/services/groups-update.service.ts
+++ src/groups/services/groups-update.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { RedisService } from "src/redis/redis.service";
+import { User } from "src/users/users.entity";
 import { Repository, UpdateResult } from "typeorm";
 import { UpdateGroupNameInputDto } from "../dto/update-group-name-input.dto";
 import { Group } from "../groups.entity";
 import { UsersGroups } from "../users_groups.entity";
+import { GroupsCreateService } from "./groups-create.service";
 import { GroupsReadService } from "./groups-read.service";
 
 
@@ -11,9 +14,18 @@ import { GroupsReadService } from "./groups-read.service";
 export class GroupsUpdateService {
 
     constructor(@InjectRepository(Group) private readonly groupRepository: Repository<Group>,
-                @InjectRepository(UsersGroups) private readonly userGroupRepo: Repository<UsersGroups>) {} 
+                @InjectRepository(UsersGroups) private readonly userGroupRepo: Repository<UsersGroups>,
+                private readonly groupsCreateService: GroupsCreateService,
+                private readonly groupsReadService: GroupsReadService,
+                private readonly redisService: RedisService) {} 
 
     async updateGroupName(uuidOfGroup, dto: UpdateGroupNameInputDto): Promise<void> {
         await this.groupRepository.update({uuid: uuidOfGroup}, {name: dto.name});
+    }
+
+    async useInviteLink(inviteKey: string, newMember: User): Promise<void> {
+        const { groupUUID } = await this.redisService.disposeInvite(inviteKey);
+        const group = await this.groupsReadService.getOneGroupByUUID(groupUUID);
+        await this.groupsCreateService.createUserGroup(newMember, group);
     }
 }
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,12 +1,33 @@
-import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
+import * as random from 'random-string-generator';
+
+export type RedisGroupInviteValue = {
+    inviteKey: string,
+    groupUUID: string,
+}
 
 @Injectable()
 export class RedisService {
 
     constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) { }
 
-    async createGroupInvite(): Promise<any> {
-        
+    async createGroupInvite(uuidOfGroup: string): Promise<RedisGroupInviteValue> {
+        const newGeneratedInviteKey: string = random(20);
+        const newInviteValue = await this.cacheManager.set<RedisGroupInviteValue>(`GroupInvite:${newGeneratedInviteKey}`, {
+            inviteKey: newGeneratedInviteKey,
+            groupUUID: uuidOfGroup,
+        })
+        return newInviteValue;
+    }
+
+    async disposeInvite(inviteKey: string): Promise<RedisGroupInviteValue> {
+
+        const inviteValue = await this.cacheManager.get<RedisGroupInviteValue>(`GroupInvite:${inviteKey}`)
+        if (!inviteValue) throw new HttpException("Such an invite either expired or doesn't exist", HttpStatus.BAD_REQUEST)
+
+        console.log("del() function of cacheManager returns:", await this.cacheManager.del(`GroupInvite:${inviteKey}`))
+
+        return inviteValue; 
     }
 }
