139ef245c654b66056acc5b7d165ca617b8d9a65
Bota <botakiller@mail.ru>
2021-08-01 18:21:24 +0300

    unnecessary imports was deleted

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,7 +1,5 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
-import { Redis } from 'ioredis';
-import { REDIS_CLIENT } from './redis.module';
 
 @Injectable()
 export class RedisService {
