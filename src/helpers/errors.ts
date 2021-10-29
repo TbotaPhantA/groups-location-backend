8c69922b4ff40f02ee7ac25b791e07122c51e74b
TbotaPhantA <botakiller@mail.ru>
2021-10-29 21:48:39 +0300

    errors file will contain all error messages

---
+++ src/helpers/errors.ts
@@ -0,0 +1,4 @@
+export const UNAUTHORIZED_EXCEPTION_MESSAGE =
+  'Invalid auth header, or token, user is not authorized';
+export const NO_GROUP_UUID_IN_PARAMS_MESSAGE =
+  'There is no UUID of group in params';
